171b0ca9f8c408c6c0b122c5d6f234a9266b7c48
Bastien <bastienrobert@outlook.com>
2018-07-31 09:00:40 +0200

    #1 Fix

--- scripts/build.js
+++ scripts/build.js
@@ -29,7 +29,7 @@ function copyPublicFolder() {
   fs.copySync(paths.appPublic, paths.appBuild, {
     dereference: true,
     filter: file => file !== paths.appHtml
-  });
+  })
 }
 
 function build() {
--- scripts/start.js
+++ scripts/start.js
@@ -9,6 +9,7 @@ const opn = require('opn')
 const ip = require('ip')
 const chalk = require('chalk')
 const WebpackDevServer = require('webpack-dev-server')
+const execSync = require('child_process').execSync
 
 const paths = require('../config/paths')
 const config = require('../config/webpack.config.dev')
@@ -16,31 +17,45 @@ const appName = require(paths.appPackageJson).name
 
 const bold = chalk.bold
 const valid = chalk.bold.green
+const progress = chalk.blue
 const error = chalk.bold.red
 const warning = chalk.keyword('orange')
+
+const clear = () => process.stdout.write('\x1B[2J\x1B[3J\x1B[H')
+
+// On server launch
+clear()
+console.log(progress('Server is loading...'))
+
+// If darwin (macOS), try to launch AppleScript with Chrome
+const shouldTryOpenChromeWithAppleScript = process.platform === 'darwin'
+
+// Message to display on each rebuild
 const message = `
 You can now view ${bold(appName)} in the browser.
 
 Local:            http://localhost:${bold(process.env.PORT)}/
-On Your Network:  http://${ip.address()}:${process.env.PORT}/
+On Your Network:  http://${ip.address()}:${bold(process.env.PORT)}/
 
 Note that the development build is not optimized.
 To create a production build, use yarn build.`
 
+// Add entry points for hot reloading
 config.entry = [
   'webpack-dev-server/client?http://localhost:8080',
   'webpack/hot/dev-server',
   ...config.entry
 ]
 
+// Create compiler using webpack configuration
 const compiler = webpack(config)
 
+// After compile
 compiler.plugin('done', callback => {
   // Clear the console
-  const clear = '\x1B[2J\x1B[3J\x1B[H'
-  const output = this.message ? clear + this.message + '\n\n' : clear
-  process.stdout.write(output)
+  clear()
 
+  // Message after clear
   callback.compilation.errors.length > 0 ||
   callback.compilation.warnings.length > 0
     ? console.log(error('Failed to compile.'))
@@ -48,6 +63,7 @@ compiler.plugin('done', callback => {
   console.log(message)
 })
 
+// Create webpack-dev-server with options
 const server = new WebpackDevServer(compiler, {
   contentBase: paths.appPublic,
   publicPath: config.output.publicPath,
@@ -61,6 +77,26 @@ const server = new WebpackDevServer(compiler, {
   stats: 'minimal'
 })
 
+// Open the server and new tab
 server.listen(process.env.PORT, 'localhost', () => {
-  opn('http://localhost:8080')
+  if (shouldTryOpenChromeWithAppleScript) {
+    try {
+      // Try our best to reuse existing tab
+      // on OS X Google Chrome with AppleScript
+      execSync(
+        `osascript openChrome.scpt "${encodeURI(
+          `http://localhost:${process.env.PORT}`
+        )}"`,
+        {
+          cwd: `${__dirname}/helpers`,
+          stdio: 'ignore'
+        }
+      )
+      return true
+    } catch (err) {
+      opn(`http://localhost:${process.env.PORT}`)
+    }
+  } else {
+    opn(`http://localhost:${process.env.PORT}`)
+  }
 })
